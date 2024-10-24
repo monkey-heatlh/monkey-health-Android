f6966af03df08f455997a42ad211468a4b869421
jsw9436 <jswjsw101010@gmail.com>
2024-10-24 14:13:47 +0900

    로그인페이지

---
+++ presentation/src/main/java/com/example/presentation/LoginPage.kt
@@ -0,0 +1,92 @@
+package com.example.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Loginpage(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(350.dp))
+
+        Text(
+            text = "MONKEY HEALTH",
+            style = TextStyle(
+                fontSize = 32.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF902BE9),
+            ),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier = Modifier
+                .width(345.dp)
+                .height(52.dp)
+                .background(color = Color(0xFFE9E9E9), shape = RoundedCornerShape(size = 12.dp)) // 배경색 및 모서리 둥글게
+                .padding(start = 24.dp, top = 12.dp, end = 24.dp, bottom = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(60.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.gauth_logo),
+                contentDescription = "Gauth Logo",
+                modifier = Modifier
+                    .height(40.dp)
+                    .width(40.dp)
+            )
+
+            Text(
+                text = "Gauth로 로그인",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color(0xFF111111),
+                ),
+                modifier = Modifier.align(Alignment.CenterVertically) // Row 내부에서 중앙 정렬
+            )
+        }
+
+        Spacer(modifier = Modifier.height(70.dp))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLoginPage() {
+    Loginpage()
+}
