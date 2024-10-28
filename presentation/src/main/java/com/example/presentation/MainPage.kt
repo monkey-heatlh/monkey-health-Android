524fe3787b75e429d90c3a6811ca1b5809879428
jsw9436 <jswjsw101010@gmail.com>
2024-10-28 14:06:29 +0900

    feature/2-ui

--- presentation/src/main/java/com/example/presentation/MainPage.kt
+++ presentation/src/main/java/com/example/presentation/MainPage.kt
@@ -1,15 +1,12 @@
 package com.example.presentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -21,13 +18,14 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun MainPage(
     modifier : Modifier = Modifier
-){
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
             .padding(20.dp),
-        horizontalAlignment = Alignment.Start
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
     ) {
         Text(
             text = "MONKEY HEALTH",
@@ -36,8 +34,24 @@ fun MainPage(
                 fontFamily = FontFamily(Font(R.font.pretendard_regular)),
                 fontWeight = FontWeight(400),
                 color = Color(0xFF902BE9),
-            )
+            ),
+            modifier = Modifier.align(Alignment.Start)
         )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "집에서 편하게\n정확한 자세로!",
+            style = TextStyle(
+                fontSize = 32.sp,
+                lineHeight = 48.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight(600),
+                color = Color(0xFF111111),
+            ),
+            modifier = Modifier.align(Alignment.Start)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
     }
 }
 @Preview
