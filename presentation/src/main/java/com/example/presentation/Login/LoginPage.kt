e3f94ac9d5eed6bf9b369729ccadafa2dd99e330
jsw9436 <jswjsw101010@gmail.com>
2024-11-18 22:18:01 +0900

    feature/4-ui

--- presentation/src/main/java/com/example/presentation/Login/LoginPage.kt
+++ presentation/src/main/java/com/example/presentation/Login/LoginPage.kt
@@ -1,4 +1,4 @@
-package com.example.presentation
+package com.example.presentation.Login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.presentation.R
 
 @Composable
 fun Loginpage(
