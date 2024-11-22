15b1ffe1ab2f63dce518f58e2f57c913740a4ba7
jsw9436 <jswjsw101010@gmail.com>
2024-11-22 11:22:02 +0900

    feature/4-ui

--- presentation/src/main/java/com/example/presentation/Main/Mainsub1.kt
+++ presentation/src/main/java/com/example/presentation/Main/Mainsub1.kt
@@ -1,25 +1,21 @@
     import androidx.compose.foundation.Image
     import androidx.compose.foundation.background
     import androidx.compose.foundation.layout.*
-    import androidx.compose.foundation.shape.RoundedCornerShape
-    import androidx.compose.material3.Button
-    import androidx.compose.material3.ButtonDefaults
     import androidx.compose.material3.Text
     import androidx.compose.runtime.Composable
     import androidx.compose.ui.Alignment
     import androidx.compose.ui.Modifier
     import androidx.compose.ui.graphics.Color
     import androidx.compose.ui.layout.ContentScale
-    import androidx.compose.ui.modifier.modifierLocalMapOf
     import androidx.compose.ui.res.painterResource
     import androidx.compose.ui.text.font.FontWeight
-    import androidx.compose.ui.text.style.TextAlign
     import androidx.compose.ui.unit.dp
     import androidx.compose.ui.unit.sp
     import com.example.presentation.R
     import androidx.compose.ui.text.TextStyle
     import androidx.compose.ui.text.font.Font
     import androidx.compose.ui.text.font.FontFamily
+    import androidx.compose.ui.text.style.TextAlign
     import androidx.compose.ui.tooling.preview.Preview
 
     @Composable
@@ -30,31 +26,45 @@
                 .background(Color.White)
                 .padding(horizontal = 16.dp, vertical = 32.dp),
             horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.Top
         ) {
-            Column(
-                modifier = Modifier.padding(1.dp)
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(10.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.arrow_down),
-                        contentDescription = "뒤로가기 이미지",
-                        contentScale = ContentScale.FillBounds,
-                        modifier = Modifier.size(48.dp)
+                Image(
+                    painter = painterResource(id = R.drawable.arrow_down),
+                    contentDescription = "뒤로가기 이미지",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.size(48.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "뒤로",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF000000),
                     )
-                    Text(
-                        text = "뒤로",
-                        style = TextStyle(
-                            fontSize = 14.sp,
-                            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
-                            fontWeight = FontWeight(400),
-                            color = Color(0xFF000000),
-                        )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top,
+            ) {
+                Text(
+                    text = "무슨 운동을 배워볼까요?",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        lineHeight = 42.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFF111111),
                     )
-                }
+                )
             }
         }
     }
