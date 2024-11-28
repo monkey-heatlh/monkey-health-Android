78f15fddf26faf897a6151e68d33a187911eb9c0
jsw9436 <jswjsw101010@gmail.com>
2024-11-28 10:53:33 +0900

    로그인 화면

--- presentation/src/main/java/com/example/presentation/Login/LoginPage.kt
+++ presentation/src/main/java/com/example/presentation/Login/LoginPage.kt
@@ -1,22 +1,23 @@
 package com.example.presentation.Login
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -27,18 +28,18 @@ import androidx.compose.ui.unit.sp
 import com.example.presentation.R
 
 @Composable
-fun Loginpage(
+fun LoginPage(
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(16.dp),
+            .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Spacer(modifier = Modifier.height(350.dp))
+        Spacer(modifier = Modifier.weight(1f))
 
         Text(
             text = "MONKEY HEALTH",
@@ -50,11 +51,60 @@ fun Loginpage(
             ),
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 64.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { /* TODO: 회원가입 기능 추가 */ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .width(345.dp)
+                    .height(51.dp)
+                    .padding(bottom = 60.dp)
+                    .border(1.dp, Color(0xFF902BE9), RoundedCornerShape(50))
+            ) {
+                Text(
+                    text = "회원가입",
+                    style = TextStyle(
+                        color = Color(0xFF902BE9),
+                        fontSize = 16.sp
+                    )
+                )
+            }
+            Button(
+                onClick = { /* TODO: 로그인 기능 추가 */ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF902BE9)
+                ),
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .width(345.dp)
+                    .height(51.dp)
+
+            ) {
+                Text(
+                    text = "로그인",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                )
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewLoginPage() {
-    Loginpage()
+    LoginPage()
 }
