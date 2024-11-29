4353a5bac495046cf15e10238c6b962c9bf996b5
jsw9436 <jswjsw101010@gmail.com>
2024-11-29 16:05:34 +0900

    Mainsub_detail1

--- presentation/src/main/java/com/example/presentation/Main/sub_detail/detail/Mainsub_detail1.kt
+++ presentation/src/main/java/com/example/presentation/Main/sub_detail/detail/Mainsub_detail1.kt
@@ -1,9 +1,85 @@
 package com.example.presentation.Main.sub_detail.detail
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.presentation.R
 
-class Mainsub_detail1 {
+@Composable
+fun Mainsub_detail1() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 16.dp, vertical = 32.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.arrow_down),
+                contentDescription = "뒤로가기 이미지",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(48.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "뒤로",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF000000),
+                )
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "PUSH UP",
+            style = TextStyle(
+                fontSize = 28.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "운동시 대흉근, 삼두근, 전면/측면삼각근\n복근, 전거근, 광배근 등의 근육군이 쓰인다.",
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight(400),
+                color = Color(0xFF000000),
+            )
+        )
+    }
 }
 @Preview
 @Composable
