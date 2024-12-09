a8fff4b044893474de621904306542cf28f165a6
jsw9436 <jswjsw101010@gmail.com>
2024-12-09 17:39:29 +0900

    SignUp1

--- presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
@@ -1,37 +1,29 @@
 package com.example.presentation.SignUp
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.presentation.R
-import com.example.presentation.Selection_Main.view.Selection_basic
 
 @Composable
 fun SignUp1(
     modifier: Modifier = Modifier
 ){
-
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 16.dp, vertical = 32.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+){}
 }
 
 @Preview
