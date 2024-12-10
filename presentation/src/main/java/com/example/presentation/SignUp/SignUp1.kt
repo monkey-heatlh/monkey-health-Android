661135e8d2087494b695b0291b0ee053038ac1e8
jsw9436 <jswjsw101010@gmail.com>
2024-12-10 10:04:24 +0900

    restart

--- presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
@@ -5,12 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.presentation.R
 
 @Composable
 fun SignUp1(
@@ -23,7 +30,9 @@ fun SignUp1(
             .padding(horizontal = 16.dp, vertical = 32.dp),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
-){}
+){
+
+    }
 }
 
 @Preview
