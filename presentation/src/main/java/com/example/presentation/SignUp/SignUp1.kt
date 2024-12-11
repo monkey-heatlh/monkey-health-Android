805c93c46b05f14e6d7ca766d236a6232195e86a
jsw9436 <jswjsw101010@gmail.com>
2024-12-11 14:19:26 +0900

    SignUp1

--- presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
@@ -1,12 +1,14 @@
 package com.example.presentation.SignUp
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,7 +24,9 @@ import com.example.presentation.R
 @Composable
 fun SignUp1(
     modifier: Modifier = Modifier
-){
+) {
+    val email = remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -30,13 +34,52 @@ fun SignUp1(
             .padding(horizontal = 16.dp, vertical = 32.dp),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
-) {
+    ) {
+        Text(
+            text = "안녕하세요!\n이메일로 가입해주세요. ",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
 
+                )
+        )
+
+        Text(
+            text = "이메일을 입력하여 함께 운동을 해보아요!!",
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight(400),
+                color = Color(0xFF000000),
+                )
+        )
+
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            label = { Text("이메일을 입력해주세요") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+        Button(
+            onClick = { /* TODO: 인증번호 발송 로직 */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF6200EE),
+                contentColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "인증번호 받기")
+        }
     }
 }
 
+
 @Preview
 @Composable
-fun SignUp1() {
+fun SignUp1Preview() {
     SignUp1()
 }
