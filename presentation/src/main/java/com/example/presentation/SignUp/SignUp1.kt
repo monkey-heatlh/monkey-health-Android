acb19abb0fe0fdc9afc6637d2e339d4c854e8190
jsw9436 <jswjsw101010@gmail.com>
2024-12-12 16:15:24 +0900

    SignUp1

--- presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/SignUp1.kt
@@ -42,38 +42,8 @@ fun SignUp1(
                 fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
                 fontWeight = FontWeight(600),
                 color = Color(0xFF000000),
-
                 )
         )
-
-        Text(
-            text = "이메일을 입력하여 함께 운동을 해보아요!!",
-            style = TextStyle(
-                fontSize = 16.sp,
-                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
-                fontWeight = FontWeight(400),
-                color = Color(0xFF000000),
-                )
-        )
-
-        OutlinedTextField(
-            value = email.value,
-            onValueChange = { email.value = it },
-            label = { Text("이메일을 입력해주세요") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp)
-        )
-        Button(
-            onClick = { /* TODO: 인증번호 발송 로직 */ },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF6200EE),
-                contentColor = Color.White
-            ),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "인증번호 받기")
-        }
     }
 }
 
