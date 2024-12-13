807eac78951acff1337b16445e9880f9342079cf
jsw9436 <jswjsw101010@gmail.com>
2024-12-13 11:04:12 +0900

    SignUp1

--- presentation/src/main/java/com/example/presentation/SignUp/view/SignUp1.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/view/SignUp1.kt
@@ -1,14 +1,9 @@
-package com.example.presentation.SignUp
+package com.example.presentation.SignUp.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
