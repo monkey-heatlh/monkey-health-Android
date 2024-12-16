d84c491dd7a277b2fc6c4763ce4ffe11a5bab4cb
jsw9436 <jswjsw101010@gmail.com>
2024-12-16 20:02:01 +0900

    SignUp3

--- presentation/src/main/java/com/example/presentation/SignUp/view/SignUp3.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/view/SignUp3.kt
@@ -26,7 +26,7 @@ fun SignUp3(
         modifier = modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(horizontal = 24.dp, vertical = 41.dp),
+            .padding(horizontal = 24.dp, vertical = 76.dp),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
     ) {
