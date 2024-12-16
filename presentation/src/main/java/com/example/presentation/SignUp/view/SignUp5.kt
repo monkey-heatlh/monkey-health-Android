54e502dd822798ef889973223787c83d791ba3e6
jsw9436 <jswjsw101010@gmail.com>
2024-12-16 19:49:01 +0900

    SignUp5

--- presentation/src/main/java/com/example/presentation/SignUp/view/SignUp5.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/view/SignUp5.kt
@@ -1,2 +1,52 @@
 package com.example.presentation.SignUp.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.presentation.R
+
+@Composable
+fun SignUp5(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(horizontal = 24.dp, vertical = 41.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ){
+        Text(
+            text = "비밀번호를 설정해주세요.",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
+
+                )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSignUp5() {
+    SignUp5()
+}
