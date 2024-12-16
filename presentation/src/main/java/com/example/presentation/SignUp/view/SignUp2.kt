7f3e0f8bf38e96572be1c413e1319cd723cdc02b
jsw9436 <jswjsw101010@gmail.com>
2024-12-16 11:54:46 +0900

    SignUp2

--- presentation/src/main/java/com/example/presentation/SignUp/view/SignUp2.kt
+++ presentation/src/main/java/com/example/presentation/SignUp/view/SignUp2.kt
@@ -59,14 +59,19 @@ fun SignUp2() {
                 placeholder = {
                     Text(
                         text = "이메일을 입력해주세요",
-                        color = Color(0xFF999999),
-                        style = TextStyle(fontSize = 14.sp)
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.pretendard_regular)),
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF7D7D7D),
+
+                            )
                     )
                 },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(50.dp)
-                    .border(1.dp, Color(0xFFB0B0B0), RoundedCornerShape(8.dp))
+                    .border(1.dp, Color(0xFF902BE9), RoundedCornerShape(8.dp))
                     .background(Color.White),
                 colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.White,
@@ -83,7 +88,7 @@ fun SignUp2() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(50.dp)
-                    .border(1.dp, Color(0xFFB0B0B0), RoundedCornerShape(8.dp)),
+                    .border(1.dp, Color(0xFF902BE9), RoundedCornerShape(8.dp)),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Transparent,
@@ -93,9 +98,11 @@ fun SignUp2() {
                 Text(
                     text = "인증번호 받기",
                     style = TextStyle(
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.SemiBold
-                    )
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_semibold)),
+                        fontWeight = FontWeight(600),
+                        color = Color(0xFFC5C5C5),
+                        )
                 )
             }
         }
